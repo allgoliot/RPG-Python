453243182c168a0c16ce215872466c4ff59a5a10
xhinthia <alexis.laurain95@gmail.com>
2019-01-28 10:57:29 +0100

    création class personnage avec pv force defense et méthode attaque

---
+++ objets/personnage.py
@@ -0,0 +1,19 @@
+#classe personnage : pv, force, defense
+## pv = 100
+## force = 10
+## defense = 5
+### méthode attaque : force 
+### méthode defense : defense*2 puis force+5 au prochain tour
+
+
+class Personnages:
+	def __init__(self,pv,force,defense):
+		self._pv = pv
+		self._force = force
+		self._defense = defense
+
+	def Attaque(self):
+		return self._force
+
+Henry = Personnages(100,10,5)
+print(Henry.Attaque())
